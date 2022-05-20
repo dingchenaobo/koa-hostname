4a189e99d52bf3cb0921224d92c15540231bb204
dingchen <dingchenaobo@163.com>
2022-05-20 11:22:19 +0800

    fix: del path ext

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ class HostnameRouter {
 
   middleware() {
     return async (context: Context, next: Next) => {
-      const { hostname, path: pathname } = context;
+      const { hostname } = context;
       const matchedKey = Array.from(this.pathDeps.keys()).find(path => {
         let matched: boolean;
         if (Array.isArray(path)) {
-          matched = path.find(p => minimatch(`${hostname}${pathname}`, p)) ? true : false;
+          matched = path.find(p => minimatch(hostname, p)) ? true : false;
         } else {
-          matched = minimatch(`${hostname}${pathname}`, path);
+          matched = minimatch(hostname, path);
         }
         return matched;
       });
